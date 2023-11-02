15f7dd8a5e244630962575cce22cee34ee2c1e8c
katsumi-watababe <ktm981124@gmail.com>
2023-11-02 16:53:36 +0900

    Vutifyインストールおよび初期設定

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+// Vuetify 使用時に必要なライブラリのインポート
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+// jsにアイコンフォント(mdi)を埋め込む場合に必要
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+
+// Vuetifyの初期設定。最低限の設定 + mdiアイコン設定
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  }
+});
+
+// use(vuetify)の追加
+createApp().use(vuetify).mount('#app');
